2888e41cc0a7488e74b4e9f1998b3bd3ae4c26e7
Emílio <andreemilio222@gmail.com>
2024-12-03 15:50:52 -0300

    Delete André_clean_code_work directory

--- André_clean_code_work/scripts/js/app.js
+++
@@ -1,6 +0,0 @@
-// app.js
-
-import { somar, subtrair } from './calculadora.js';
-
-console.log("Soma de 5 e 3:", somar(5, 3));       // Saída: 8
-console.log("Subtração de 10 e 4:", subtrair(10, 4)); // Saída: 6
